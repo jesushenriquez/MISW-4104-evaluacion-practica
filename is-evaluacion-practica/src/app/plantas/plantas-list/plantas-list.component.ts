e31790d97392d93d5782a96781a1a859c7781272
Jesus Henriquez <ja.henriquez@uniandes.edu.co>
2023-04-30 17:03:37 -0500

    feature: punto 2 - se agregan las variables para mostrar las plantas de interior y exterior

--- is-evaluacion-practica/src/app/plantas/plantas-list/plantas-list.component.ts
+++ is-evaluacion-practica/src/app/plantas/plantas-list/plantas-list.component.ts
@@ -10,9 +10,13 @@ import { PlantasService } from '../plantas.service';
 export class PlantasListComponent {
 
   public plants: Planta[];
+  public indoorPlants: Number;
+  public outdoorPlants: Number;
 
   constructor(public plantasService: PlantasService) {
     this.plants = [];
+    this.indoorPlants = 0;
+    this.outdoorPlants = 0;
   }
 
   ngOnInit(): void {
@@ -22,7 +26,9 @@ export class PlantasListComponent {
   private getPlants(){
     this.plantasService.getAll().subscribe((plants:Planta[]) => {
       this.plants = plants;
-    })
+      this.indoorPlants = this.plants.filter(plant => plant.tipo === 'Interior').length;
+      this.outdoorPlants = this.plants.filter(plant => plant.tipo === 'Exterior').length;
+    });
   }
 
 }
